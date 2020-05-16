928adf23a20a3d45445791c06d88c359a7d8db25
Remi Beaupre <remi.beaupre@ara-uas.com>
2020-05-16 16:00:46 -0400

    add cmake and stuff

--- setup/apps.sh
+++ setup/apps.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt-get install -y filezilla wine-stable lcov speedcrunch htop ccache ninja-build
+sudo apt-get install -y \
+    filezilla \ 
+    wine-stable \
+    lcov \
+    speedcrunch \
+    htop \
+    ccache \
+    ninja-build \
+    cmake
+
 sudo snap install slack --classic
 sudo snap install clion --classic --edge
--- setup/libraries.sh
+++ setup/libraries.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt-get install -y libx11-dev libusb-1.0.0-dev xorg-dev libglu1-mesa-dev freeglut3-dev
+sudo apt-get install -y \
+    libx11-dev \
+    libusb-1.0.0-dev \
+    xorg-dev \
+    libglu1-mesa-dev \
+    freeglut3-dev \
+    libudev-dev
