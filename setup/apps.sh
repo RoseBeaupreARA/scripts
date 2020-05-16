47aa9c1337c6b326188f416b4f14db7bebeb4931
Remi Beaupre <remi.beaupre@ara-uas.com>
2020-05-16 13:24:08 -0400

    ars

--- setup/apps.sh
+++ setup/apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt-get install -y filezilla wine-stable lcov speedcrunch htop
+sudo apt-get install -y filezilla wine-stable lcov speedcrunch htop ccache ninja-build
 sudo snap install slack --classic
 sudo snap install clion --classic --edge
